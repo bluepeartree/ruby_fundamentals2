d5489024e14705fb101a82cf34ed0f04e585228f
bluepeartree <sherrie.sebastian@gmail.com>
2017-11-08 21:34:02 -0500

    exercise 2 & 3 complete

--- 01 - Reinforcing Exercises: Programming Fundamentals/person.rb
+++ 01 - Reinforcing Exercises: Programming Fundamentals/person.rb
@@ -1,1 +1,38 @@
-Emotions = {:anger => 3, :confusion => 3, :stress => 3 }
+Emotions = {:anger => 1, :confusion => 2, :stress => 3 }
+
+class Person
+
+def initialize(name)
+  @name = name
+  @Emotions = Emotions
+end
+
+def name
+  @name
+end
+
+def Emotions
+  @Emotions
+end
+
+def name=(name)
+  @name = name
+end
+
+def feelings
+  @Emotions.map do |key, value|
+        if value == 1
+          puts "I am feeling a low amount of #{key}."
+        elsif value == 2
+          puts "I am feeling a medium amount of #{key}."
+        else
+          puts "I am feeling a high amount of #{key}."
+        end
+  end
+end
+
+end
+
+Idiot = Person.new("Idiot")
+
+Idiot.feelings
