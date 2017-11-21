a70125e8bc948945d6cf65153dae1cd728274c76
bluepeartree <sherrie.sebastian@gmail.com>
2017-11-21 13:14:10 -0500

    adapted so only picks 3 cards

--- cards.rb
+++ cards.rb
@@ -1,4 +1,5 @@
 def select_cards(possible_cards, hand)
+  loop do
 
   possible_cards.each do |current_card|
     print "Do you want to pick up #{current_card}?"
@@ -6,13 +7,14 @@ def select_cards(possible_cards, hand)
     if answer.downcase == 'y'
       hand << current_card
     end
-
+    break if hand.length==3
+  end
+  break if hand.length==3
   end
   puts "Your new hand is"
   return hand
 end
 
 available_cards = ['queen of spades', '2 of clubs', '3 of diamonds', 'jack of spades', 'queen of hearts']
-
 
 puts new_hand = select_cards(available_cards, [])
