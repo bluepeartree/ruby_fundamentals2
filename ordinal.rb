28b9f8a7e8a788a8ba8305d0b039708fbe414534
bluepeartree <sherrie.sebastian@gmail.com>
2017-11-29 12:07:02 -0500

    ordinal indicator generated for all numbers

---
+++ ordinal.rb
@@ -0,0 +1,31 @@
+# numbers = (1..20).to_a
+#
+# ordinal_numbers = []
+#
+# numbers.each do |number|
+#   if number == 1
+#     ordinal_number = number.to_s + "st"
+#   elsif number == 2
+#     ordinal_number = number.to_s + "nd"
+#   elsif number == 3
+#     ordinal_number = number.to_s + "rd"
+#   else
+#     ordinal_number = number.to_s + "th"
+#   end
+#   ordinal_numbers << ordinal_number
+# end
+
+puts "Please enter an integer:"
+number = gets.to_i
+
+if number.digits[0] == 1
+  ordinal_number = number.to_s + "st"
+elsif number.digits[0] == 2
+  ordinal_number = number.to_s + "nd"
+elsif number.digits[0] == 3
+  ordinal_number = number.to_s + "rd"
+else
+  ordinal_number = number.to_s + "th"
+end
+
+puts ordinal_number
