83dfe7dd9e4bd5d48227f0651be349f2c10e6830
bluepeartree <sherrie.sebastian@gmail.com>
2017-12-06 23:16:28 -0500

    replicated required output

---
+++ lowercase.rb
@@ -0,0 +1,14 @@
+words = ["Totam", "ut", "odit","quis", "Maiores", "unde", "EX", "EST", "corporis"]
+
+words.each do |word|
+  check_word = word.downcase!
+  if check_word == nil && word.length > 4
+    puts "#{word.downcase!}long and lowercase"
+  elsif check_word == nil && word.length <= 4
+    puts "lowercase"
+  elsif word.length > 4 &&  check_word != nil
+    puts "long"
+  else
+    puts word.upcase!
+  end
+end
